a5d0165464542ba376c5de3dee6572aae53e671f
Leonardo Silvatti <leosilvatti0@gmail.com>
2023-06-04 18:46:24 -0300

    wip: just testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import KeyBoard from 'components/KeyBoard/KeyBoard';
 
 function App() {
+  const [keyPressed, setKeyPressed] = useState<string | null>(null);
+  const handleKeyPressed = (key:string) => {
+    setKeyPressed(key);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {keyPressed}
+        <KeyBoard handleKeyPressed={(key: string) => handleKeyPressed(key)}></KeyBoard>
     </div>
   );
 }
